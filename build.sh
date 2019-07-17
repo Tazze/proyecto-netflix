0f286464d45f3a9dc97888bcd81977d75e5a536f
Santiago <tazzeoptical@gmail.com>
2019-07-17 11:59:11 +0200

    restored tests for mvc and restjpa, modified build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-#CLEANING
-mvn clean -f config/pom.xml
-mvn clean -f eureka/pom.xml
-mvn clean -f modelo/pom.xml
-mvn clean -f mvc/pom.xml
-mvn clean -f restjpa/pom.xml
-#BUILDING
-mvn package -f config/pom.xml
-mvn package -f eureka/pom.xml
-mvn install -f modelo/pom.xml
-mvn package -f mvc/pom.xml
-mvn package -f restjpa/pom.xml
+#MAVEN
+mvn clean package -DskipTests -f config/pom.xml
+mvn clean package -DskipTests -f eureka/pom.xml
+mvn clean install -DskipTests -f modelo/pom.xml
+mvn clean package -DskipTests -f mvc/pom.xml
+mvn clean package -DskipTests -f restjpa/pom.xml
 #DOCKER
 docker build --rm -f "config/Dockerfile" -t config:latest config
 docker build --rm -f "eureka/Dockerfile" -t eureka:latest eureka
